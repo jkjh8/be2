071f3312107a5d9dda92be76653cf1663e0cbd5c
jkjh8 <jkjh82@naver.com>
2021-11-18 12:50:05 +0900

    files

--- api/files/index.js
+++ api/files/index.js
@@ -6,13 +6,15 @@ exports.get = async (req, res) => {
   const { folder } = req.body
   let remotePath
   let currentPath
-  if (folder && folder !== 'undefined') {
+  if (folder && folder.length && folder !== 'undefined') {
     remotePath = folder.join('/')
     currentPath = path.join(filesPath, remotePath)
   } else {
     remotePath = ''
     currentPath = path.join(filesPath)
   }
+
+  console.log(folder, currentPath)
 
   try {
     const rt = []
@@ -61,5 +63,24 @@ exports.get = async (req, res) => {
     res.status(200).json({ files: rt, path: folder })
   } catch (err) {
     logger.error(`파일 읽기 에러 ${err}`)
+    res.status(500).json({ error: err, message: '폴더를 읽을 수 없습니다' })
+  }
+}
+
+module.exports.createFolder = async (req, res) => {
+  try {
+    console.log(req.body)
+    const currentPath = path.join(filesPath, req.body.folder.join('/'))
+
+    if (fs.existsSync(currentPath)) {
+      console.log('폴더 있음')
+    }
+
+    logger.info(
+      `폴더가 생성되었습니다. user: ${req.user.email}, folder: ${currentPath}`
+    )
+  } catch (err) {
+    logger.error(`폴더 생성 에러 ${err}`)
+    res.staus(500).json({ error: err, message: '폴더를 생성할 수 없습니다' })
   }
 }
