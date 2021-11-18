7fddbe5f553dd3bdb03946488a6131dc16c2ee34
jkjh8 <jkjh82@naver.com>
2021-11-18 21:12:50 +0900

    files2

--- api/files/index.js
+++ api/files/index.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 const logger = require('config/logger')
+const eventlog = require('api/eventlog')
 
 exports.get = async (req, res) => {
   const { folder } = req.body
@@ -62,25 +63,52 @@ exports.get = async (req, res) => {
     }
     res.status(200).json({ files: rt, path: folder })
   } catch (err) {
-    logger.error(`파일 읽기 에러 ${err}`)
+    logger.error(`파일 읽기 - 서버 에러 ${err}`)
     res.status(500).json({ error: err, message: '폴더를 읽을 수 없습니다' })
   }
 }
 
 module.exports.createFolder = async (req, res) => {
   try {
-    console.log(req.body)
-    const currentPath = path.join(filesPath, req.body.folder.join('/'))
+    const { folder, name } = req.body
+    const { email } = req.user
+    const currentPath = path.join(filesPath, folder.join('/'))
+    const target = path.join(filesPath, folder.join('/'), name)
 
     if (fs.existsSync(currentPath)) {
-      console.log('폴더 있음')
+      if (fs.existsSync(target)) {
+        eventlog.warning({
+          source: email,
+          message: `폴더 생성 - 중복 폴더 Path: ${targert}`
+        })
+        logger.warning(`폴더 생성 - 중복 폴더, user:${email}, path: ${target}`)
+        res.status(500).json({ error: null, message: '중복 폴더' })
+      } else {
+        fs.mkdirSync(target)
+        logger.info(`폴더 생성 - 완료 user:${email}, path: ${target}`)
+        res.status(200).json({ message: '생성완료', path: target })
+      }
+    } else {
+      eventlog.error({
+        source: email,
+        message: `폴더 생성 - 위치없음 Path: ${currentPath}}`
+      })
+      logger.error(`폴더 생성 - 위치없음. user: ${email}, path: ${currentPath}`)
+      res.status(500).json({
+        error: null,
+        message: '생성할 폴더의 위치가 존재 하지 않습니다'
+      })
     }
 
-    logger.info(
-      `폴더가 생성되었습니다. user: ${req.user.email}, folder: ${currentPath}`
-    )
+    logger.info(`폴더가 생성되었습니다. user: ${email}, path: ${currentPath}`)
   } catch (err) {
-    logger.error(`폴더 생성 에러 ${err}`)
-    res.staus(500).json({ error: err, message: '폴더를 생성할 수 없습니다' })
+    logger.error(`폴더 생성 - 서버 에러 ${err}`)
+    res.status(500).json({ error: err, message: '폴더를 생성할 수 없습니다' })
   }
+}
+
+module.exports.delete = async function (req, res) {
+  console.log(req.body)
+
+  res.sendStatus(200)
 }
--- routes/files/index.js
+++ routes/files/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
+const { isLoggedIn } = require('api/users/checkLoggedIn')
 const files = require('api/files')
 
 router.post('/get', files.get)
+router.post('/createFolder', isLoggedIn, files.createFolder)
+router.post('/delete', isLoggedIn, files.delete)
 
 module.exports = router
