6e6517b82a1d5f34e088a3b5259542e01f4df6ff
jkjh8 <jkjh82@naver.com>
2021-11-25 23:35:08 +0900

    사용자 확인 로직 변경

--- api/users/checkLoggedIn.js
+++ api/users/checkLoggedIn.js
@@ -3,19 +3,16 @@ const logger = require('config/logger')
 
 module.exports.isLoggedIn = (req, res, next) => {
   passport.authenticate('access', { session: false }, (err, user) => {
-    try {
-      if (err) {
-        logger.error(`로그인 체크 - 인증 에러 - ${err} `)
-        return res.status(500).redirect('/').send('server error')
-      }
-      if (user) {
-        req.user = user
-        next()
-      } else {
-        res.status(401).redirect('/').send('no Tokens')
-      }
-    } catch (err) {
-      logger.error(`로그인 체크 - 서버 에러 - ${err}`)
+    console.log(err, user)
+    if (err) {
+      logger.error(`로그인 체크 - 인증 에러 - ${err} `)
+      return res.sendStatus(500)
+    }
+    if (user) {
+      req.user = user
+      next()
+    } else {
+      return res.sendStatus(401)
     }
   })(req, res, next)
 }
